8357ef7dafd6b53d6610673f3964ef87d36a9d51
sky <sky@cubietech.com>
2014-06-19 20:47:41 +0800

    my git

---
+++ a.c
@@ -0,0 +1,8 @@
+#include<stdio.h> 
+#define max 1000 
+
+int main(void) 
+{ 
+        printf( "hello world,%d\n",max); 
+        return 0; 
+}
---
+++ b.c
@@ -0,0 +1,8 @@
+#include<stdio.h> 
+#define max 1000 
+
+int main(void) 
+{ 
+	printf("abcde");
+        return 0; 
+}
